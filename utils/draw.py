86f5182020333d7b01f0cc289a436e32b7ee52de
satyabratadas <satyabratadas996@gmail.com>
2026-04-12 07:16:36 -0400

    live camera issue fix and dwar pose color updated

--- utils/draw.py
+++ utils/draw.py
@@ -5,9 +5,26 @@ mp_drawing = mp.solutions.drawing_utils
 
 def draw_pose(frame, results):
     if results.pose_landmarks:
+
+        # 🔴 Landmarks (points)
+        landmark_style = mp_drawing.DrawingSpec(
+            color=(0, 0, 255),   # GREEN (BGR)
+            thickness=4,         # line thickness
+            circle_radius=6      # 🔥 increase size here
+        )
+
+        # 🔵 Connections (lines)
+        connection_style = mp_drawing.DrawingSpec(
+            color=(0, 255, 255),   # BLUE
+            thickness=3,
+            circle_radius=2
+        )
+
         mp_drawing.draw_landmarks(
             frame,
             results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=landmark_style,
+            connection_drawing_spec=connection_style
         )
     return frame
